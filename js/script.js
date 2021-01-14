ba316bcbc6216e28c711d507982255cba7583293
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-14 16:36:23 +0100

    aggiustamenti

--- js/script.js
+++ js/script.js
@@ -4,33 +4,29 @@
 //Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-//genero dei numeri random assegnandoli ad un array
+//creo i 2 array e genero dei numeri random assegnandoli a quelle dell'alert
 var array5Random = [];
 var arrayUtente = [];
 for(var i = 0 ; i < 5; i++){
   array5Random.push(generaNumero(1,100));
 }
 alert(array5Random);
+
+// dopo la chiusura dell'alert parte il timeout di 3 secondi coi 5 comparePrompt
+
 setTimeout(comparePrompt, 3000);
 
-function comparePrompt(){
-  for(var i = 0 ; i < 5; i++){
-    arrayUtente.push(parseInt(prompt("inserisci un numero")));
-  }
-}
 
 console.log(array5Random);
 console.log(arrayUtente);
 
-
-// 4 L'utente per 30 secondi non vede nulla....e quindi cercherà immagino di ripetersi mentalmente i numeri
-
-// 5 L'utente prova a inserire, uno alla volta, tutti e 5 i numeri :muscle:
-
-
-
-
 //Funzione genera numero casuale compreso tra min e max
 function generaNumero(min, max){
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// qui la funzione per far comparire il prompt
+function comparePrompt(){
+  for(var i = 0 ; i < 5; i++){
+    arrayUtente.push(parseInt(prompt("inserisci un numero")));
+  }
 }
